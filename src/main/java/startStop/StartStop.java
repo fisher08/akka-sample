7d4c7a2fe456aaed4f172a1e6841bd0258cdff40
fisher08 <rbhbkk08>
2018-03-03 09:24:26 +0200

    Test pre hook

---
+++ src/main/java/startStop/StartStop.java
@@ -0,0 +1,16 @@
+package startStop;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+/**
+ * Created by Kirill on 02.03.2018.
+ */
+public class StartStop {
+    public static void main(String[] args) {
+        final ActorSystem system = ActorSystem.create("helloakka");
+        ActorRef first = system.actorOf(Props.create(StartStopActor1.class), "first");
+        first.tell("stop", ActorRef.noSender());
+    }
+}
